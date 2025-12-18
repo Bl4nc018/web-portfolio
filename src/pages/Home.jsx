93732a8f6cc0c1cc3e20c56a44de7e7b759afbc0
Bl4nc018 <abr04work@gmail.com>
2025-12-18 18:34:34 +0100

    feat: add Navbar component and integrate it into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "@/components/Navbar";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -8,9 +9,8 @@ export const Home = () => {
         <ThemeToggle/>
         {/* Background Effects */}
         <StarBackground />
-
         {/* Navbar */}
-
+        <Navbar />
         {/* Main Content */}
 
 
