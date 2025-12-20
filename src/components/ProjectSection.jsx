264bdfb3a78a4953f7a34577e78e966ce89c213a
Bl4nc018 <abr04work@gmail.com>
2025-12-20 21:45:59 +0100

    feat: enhance ProjectSection with project details display and GitHub link

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,3 +1,5 @@
+import { Github } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 const projects = [
     {
@@ -44,10 +46,48 @@ export const ProjectSection = () => {
                         className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
                     >
                         <div className="h-48 overflow-hidden">
-                            <img src={project.image} alt={project.title} className="w-full h-full object-coverA"/>
+                            <img 
+                                src={project.image} 
+                                alt={project.title} 
+                                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
+                            />
+                        </div>
+
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+
+                            <h3 className="text-xl font-semibold mb-1"> {project.title} </h3>
+                            <p className="text-muted-foreground text-sm mb-4"> {project.description} </p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a 
+                                        href={project.githubUrl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    >
+                                        <Github/>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ))}
+            </div>
+
+            <div className="text-center mt-12">
+                <a 
+                    className="cosmic-button w-fit flex items-center mx-auto gap-2" 
+                    target="_blank"
+                    href="https://github.com/Bl4nc018"
+                >
+                    Check My Github <ArrowRight size={16}/>
+                </a>
             </div>
         </div>
     </section>
