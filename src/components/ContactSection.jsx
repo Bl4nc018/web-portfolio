87aefe0e42ac966687b8c79c88d8dc167c8ff5b5
Bl4nc018 <abr04work@gmail.com>
2025-12-22 21:03:35 +0100

    fix: improve responsive layout of contact cards

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,7 +3,7 @@ import { Github, Linkedin, Mail, MapPin } from "lucide-react";
 export const ContactSection = () => {
 
     return (
-        <section id="contact" className="py-20 px-4 bg-secondary/30">
+        <section id="contact" className="py-30 px-4 bg-secondary/30">
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:text-5xl font-bold mb-6 text-center">
                     Contacta <span className="text-primary"> Conmigo </span>
@@ -17,20 +17,20 @@ export const ContactSection = () => {
                         <div className="flex flex-col p-4 sm:flex-row justify-center gap-10">
 
                             {/* Email Card */}
-                            <div className="bg-card w-80 p-4 rounded-lg shadow-xs flex items-center space-x-4">
+                            <div className="bg-card w-auto p-4 rounded-lg shadow-xs flex items-center space-x-4">
                                 <div className="p-3 rounded-full bg-primary/10">
                                     <Mail className="h-6 w-6 text-primary" />
                                 </div>
                                 <a
                                     href="mailto:abr04work@gmail.com"
-                                    className="text-muted-foreground hover:text-primary transition-colors text-xl"
+                                    className="text-muted-foreground hover:text-primary transition-colors text-base sm:text-xl"
                                 >
                                     abr04work@gmail.com
                                 </a>
                             </div>
 
                             {/* Location Card */}
-                            <div className="bg-card w-60 p-4 rounded-lg shadow-xs flex items-center space-x-4">
+                            <div className="bg-card w-auto p-4 rounded-lg shadow-xs flex items-center space-x-4">
                                 <div className="p-3 rounded-full bg-primary/10">
                                     <MapPin className="h-6 w-6 text-primary" />
                                 </div>
